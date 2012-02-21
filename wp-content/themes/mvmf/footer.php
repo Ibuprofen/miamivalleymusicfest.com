6d004bd03f9fa86a4930fef229dd882501399e8c
Tony Rieker <tony@plenary.org>
2012-02-21 13:46:27 -0800

    Adding MVMF

---
+++ wp-content/themes/mvmf/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+
+<hr />
+<div id="footer">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		Miami Valley Music Festival Association &copy;2008-<?php echo date('Y'); ?> | <?php wp_loginout(); ?>
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+</div>
+</div>
+
+<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
+<?php /* "Just what do you think you're doing Dave?" */ ?>
+
+		<?php wp_footer(); ?>
+<!-- Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9724052-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- /Analytics -->
+</body>
+</html>
---
+++ wp-content/themes/mvmf/header.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<style type="text/css" media="screen">
+
+<?php
+// Checks to see whether it needs a sidebar or not
+
+if ( !empty($withcomments) && !is_single() ) {
+?>
+	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbg-<?php bloginfo('text_direction'); ?>.jpg") repeat-y top; border: none; }
+<?php } else { // No sidebar ?>
+	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/wide.gif") repeat-y top; border: none; }
+<?php } ?>
+
+</style>
+
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+<?php wp_head(); ?>
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/jqueryslidemenu.css" />
+
+<!--[if lte IE 7]>
+<style type="text/css">
+html .jqueryslidemenu{height: 1%;} /*Holly Hack for IE7 and below*/
+</style>
+<![endif]-->
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/jqueryslidemenu.js"></script>
+</head>
+<body>
+<div id="page">
+
+<div id="header">
+	<div id="headerimg">
+	</div>
+</div>
+	<div id="jqueryslidemenucontainer">
+		<div id="myslidemenu" class="jqueryslidemenu">
+			<ul>
+				<li style="border-left: 1px solid #A09577;"><a href="http://www.miamivalleymusicfest.com/">Home</a></li>
+				<li><a href="#">Music &amp; Arts</a>
+					<ul>
+						<li><a href="/lineup/">Lineup 2011</a></li>
+						<li><a href="/picture-gallery/">Picture Gallery</a></li>
+						<li><a href="/vendors/">Vendors</a></li>
+						<li><a href="/potential-artists/">Potential Artists</a></li>
+						<li><a href="/past-artists/">Past Artists</a></li>
+					</ul>
+				</li>
+				<li><a href="/tickets/">Tickets</a></li>
+				<li><a href="#">Location</a>
+					<ul>
+						<li><a href="/camping/">Camping</a></li>
+						<li><a href="/rules/">Rules</a></li>
+						<li><a href="/directions/">Directions</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Sponsors</a>
+					<ul>
+						<li><a href="/sponsors/">Sponsors 2011</a></li>
+						<li><a href="/donate/">Donate</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Charity Rocks</a>
+					<ul>
+						<li><a href="/volunteer/">Volunteer</a></li>
+						<li><a href="/interested-charity-orgs/">Interested Charity Orgs</a></li>
+						<li><a href="/charities/">Charities 2011</a></li>
+						<li><a href="/past-charities/">Past Charities</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contact</a>
+					<ul>
+						<li><a href="/press-kit/">Press Kit</a></li>
+						<li><a href="/contact-us/">Contact Us</a></li>
+					</ul>
+				</li>
+				<li><a href="#">MVMFA</a>
+					<ul>
+						<li><a href="/mission/">Mission</a></li>
+						<li><a href="/members/">Members</a></li>
+						<li><a href="/friends/">Friends</a></li>
+					</ul>
+				</li>
+			</ul>
+			<br style="clear: left" />
+		</div>
+	</div>
+<hr />
